4d5829eb6960a22a75416fb47a8287fae7de04a4
Arpad Borsos <arpad.borsos@googlemail.com>
2013-03-13 15:12:26 +0100

    better code to patch up syntax errors

--- test/completion.js
+++ test/completion.js
@@ -1,3 +1,5 @@
+var should = require('should');
+
 var Completion = require('../').Completion;
 
 describe('Completion', function () {
@@ -41,5 +43,85 @@ describe('Completion', function () {
 			c._inComment(3).should.be.true;
 			c._inComment(4).should.be.false;
 		});
+	});
+
+	describe('Literal detection', function () {
+		it('should detect strings', function () {
+			var c = new Completion({
+				source: '"string".prop'
+			});
+			c._inLiteral(0).should.be.false;
+			c._inLiteral(1).should.be.true;
+			c._inLiteral(7).should.be.true;
+			c._inLiteral(8).should.be.false;
+		});
+		it('should detect booleans', function () {
+			var c = new Completion({
+				source: 'true.prop'
+			});
+			c._inLiteral(0).should.be.false;
+			c._inLiteral(1).should.be.true;
+			c._inLiteral(3).should.be.true;
+			c._inLiteral(4).should.be.false;
+		});
+		it('should detect regexp', function () {
+			var c = new Completion({
+				source: '/ab/.prop'
+			});
+			c._inLiteral(0).should.be.false;
+			c._inLiteral(1).should.be.true;
+			c._inLiteral(3).should.be.true;
+			c._inLiteral(4).should.be.false;
+		});
+	});
+
+	describe('Identifier detection', function () {
+		it('should find the identifier when offset is in it', function () {
+			var c = new Completion({
+				source: 'ident'
+			});
+			c._identifierAt(0).should.include({value: 'ident'});
+		});
+		it('should find the identifier when offset is after it', function () {
+			var c = new Completion({
+				source: 'o     .       prop'
+			});
+			c._identifierAt(3).should.include({value: 'o', range: [0,1]});
+		});
+		it('should find the identifier when offset is before it', function () {
+			var c = new Completion({
+				source: 'o .       prop'
+			});
+			c._identifierAt(4).should.include({value: 'prop', range: [10,14]});
+		});
+		it('should return null when no identifier present', function () {
+			var c = new Completion({
+				source: 'if (ident) {}'
+			});
+			should.not.exist(c._identifierAt(2));
+			c._identifierAt(9).should.include({value: 'ident', range: [4, 9]});
+			should.not.exist(c._identifierAt(10));
+		});
+	});
+
+	it('should fix stale dots in a statement', function () {
+			var c = new Completion({
+				source: '(10).  \nif (true) {}'
+			});
+			var fixed = c._fixDot(7);
+			fixed.body[1].should.include({type: 'IfStatement'});
+	});
+
+	it('should fix stale dots in an expression', function () {
+			var c = new Completion({
+				source: 'if (obj.   \n) {}'
+			});
+			var fixed = c._fixDot(12);
+			fixed.body[0].should.include({type: 'IfStatement'});
+	});
+
+	describe('autocompletion', function () {
+		it('should gracefully handle syntax errors');
+		it('should complete global variables');
 	});
 });
